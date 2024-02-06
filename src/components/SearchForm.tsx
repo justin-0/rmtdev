bc1d3fde79e5c3e9b64edf5e77b0146a95ecfba9
Justin OConnell <itsjustinoconnell@gmail.com>
2024-02-06 12:51:18 +0000

    feat: search form input controlled with state

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
+
 export default function SearchForm() {
+  const [search, setSearch] = useState("");
+
   return (
-    <form action="#" className="search">
+    <form onSubmit={(e) => e.preventDefault()} className="search">
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
         spellCheck="false"
-        type="text"
+        type={search}
         required
         placeholder="Find remote developer jobs..."
+        onChange={(e) => setSearch(e.target.value)}
       />
     </form>
   );
